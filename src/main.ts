ec5704d87967a39e3f4fc5743148e3236e40c63c
Usamahafiz8 <usamahafiz8@gmail.com>
2025-09-23 01:34:28 +0500

    update cors

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  // Enable CORS
+  // Enable CORS for all origins
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002'],
+    origin: true, // Allow all origins
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
